af2a8adfe753093a69c369fee4e85ba059ce0e0c
Volgin7 <8612020@gmail.com>
2023-02-13 13:26:55 +0300

    +task5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
     public static void task1 () {
         var dog = 8.0;
@@ -52,5 +53,17 @@ public class Main {
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+    }
+    public static void task5 () {
+        var frog = 3.5;
+
+        System.out.println("Задача 5");
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog +4;
+        System.out.println(frog);
     }
 }
