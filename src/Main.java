812cfe9cc295a8b25fd06092b2cd826eecb9dd48
Volgin7 <8612020@gmail.com>
2023-02-13 13:13:20 +0300

    +task3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static void task1 () {
         var dog = 8.0;
@@ -23,6 +24,20 @@ public class Main {
         paper = paper + 4;
 
         System.out.println("Задача 2");
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+    public static void task3 () {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+
+        System.out.println("Задача 3");
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
